795312333dc58bf9a0fd0235bbca08c0c23025f1
Caleb James Smith <colorado.caleb.smith@gmail.com>
2025-02-04 15:22:44 -0600

    Write plotHist() method. Use variable for input file.

--- python/make_plots.py
+++ python/make_plots.py
@@ -5,17 +5,20 @@ import matplotlib.pyplot as plt
 import tools
 
 def main():
-
-    plot_dir = "plots"
+    input_file = "egamma_test_2025_02_03_run_2/EGamma0_Run2023C-22Sep2023_v1-v1_NANOAOD/get_1d_pt_eta_phi_tnp_histograms_1/HLT_Ele30_WPTight_Gsf_histos.pkl"
     output_file = "mll_vs_pt_barrel_passing"
+    plot_dir = "plots"
     
     tools.makeDir(plot_dir)
 
-    with open("egamma_test_2025_02_03_run_2/EGamma0_Run2023C-22Sep2023_v1-v1_NANOAOD/get_1d_pt_eta_phi_tnp_histograms_1/HLT_Ele30_WPTight_Gsf_histos.pkl", "rb") as f:
+    with open(input_file, "rb") as f:
         histos = pickle.load(f)
     
     hist = histos['pt']['barrel']['passing']
-    
+
+    plotHist(hist, plot_dir, output_file)
+
+def plotHist(hist, plot_dir, output_file):
     # Create a figure and an axes object
     fig, ax = plt.subplots()
     hist.plot()
