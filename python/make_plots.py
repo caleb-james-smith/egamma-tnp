98cf55b68f08743ec86df942e3b9c72bdbde3b4d
Caleb James Smith <colorado.caleb.smith@gmail.com>
2025-02-04 15:54:58 -0600

    Write plotHistCollection() method. Remove old code.

--- python/make_plots.py
+++ python/make_plots.py
@@ -5,23 +5,15 @@ import matplotlib.pyplot as plt
 import tools
 
 def main():
-    input_file = "egamma_test_2025_02_03_run_2/EGamma0_Run2023C-22Sep2023_v1-v1_NANOAOD/get_1d_pt_eta_phi_tnp_histograms_1/HLT_Ele30_WPTight_Gsf_histos.pkl"
-    output_file = "mll_vs_pt_barrel_passing"
     plot_dir = "plots"
-    
     tools.makeDir(plot_dir)
-
+    
+    input_file = "egamma_test_2025_02_03_run_2/EGamma0_Run2023C-22Sep2023_v1-v1_NANOAOD/get_1d_pt_eta_phi_tnp_histograms_1/HLT_Ele30_WPTight_Gsf_histos.pkl"
     with open(input_file, "rb") as f:
         histCollection = pickle.load(f)
     
-    #print("histCollection:")
-    #print(histCollection)
-
     printKeys(histCollection)
-
-    hist = histCollection['pt']['barrel']['passing']
-
-    plotHist(hist, plot_dir, output_file)
+    plotHistCollection(histCollection, plot_dir)
 
 def printKeys(histCollection):
     print("Keys:")
@@ -32,6 +24,14 @@ def printKeys(histCollection):
             for key_3 in histCollection[key_1][key_2]:
                 print("   - {0}".format(key_3))
 
+def plotHistCollection(histCollection, plot_dir):
+    for key_1 in histCollection:
+        for key_2 in histCollection[key_1]:
+            for key_3 in histCollection[key_1][key_2]:
+                hist = histCollection[key_1][key_2][key_3]
+                output_file = "mll_vs_{0}_{1}_{2}".format(key_1, key_2, key_3)
+                plotHist(hist, plot_dir, output_file)
+
 def plotHist(hist, plot_dir, output_file):
     # Create a figure and an axes object
     fig, ax = plt.subplots()
@@ -39,11 +39,12 @@ def plotHist(hist, plot_dir, output_file):
     
     output_png = "{0}/{1}.png".format(plot_dir, output_file)
     output_pdf = "{0}/{1}.pdf".format(plot_dir, output_file)
-
+    
+    # Save figure to output files (png and pdf)
     plt.savefig(output_png, bbox_inches='tight')
     plt.savefig(output_pdf, bbox_inches='tight')
 
-    # close to avoid memory warning 
+    # Close to avoid memory warning 
     plt.close('all')
 
 if __name__ == "__main__":
